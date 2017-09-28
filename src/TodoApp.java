afcb96949da3e4fc429105cf1b6d2c75a600636d
Viktor Salanki <salankiv5@gmail.com>
2017-09-28 15:48:38 +0200

    adding listing method

--- src/TodoApp.java
+++ src/TodoApp.java
@@ -12,7 +12,7 @@ public class TodoApp {
 		if (args.length == 0) {
 			greetings();
 		} else if (args[0].equals("-l")) {
-			System.out.println("listing tasks");
+			listing();
 		} else if (args[0].equals("-a")) {
 			System.out.println("adding task");
 		} else if (args[0].equals("-r")) {
@@ -35,6 +35,20 @@ public class TodoApp {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	public static void listing() {
+		Path taskFile = Paths.get("/home/salankiv/greenfox/salankiv-todo-app/assets/tasks.txt");
+
+		try {
+			List<String> lines = Files.readAllLines(taskFile);
+			for (int i = 0; i < lines.size(); i++) {
+				System.out.println((i + 1) + ". " + lines.get(i));
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 }
