bb41d7783c513745b4aacf9ce88390c704c3bb93
Viktor Salanki <salankiv5@gmail.com>
2017-09-28 15:37:18 +0200

    adding argument check

--- src/TodoApp.java
+++ src/TodoApp.java
@@ -3,11 +3,24 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Scanner;
 
 public class TodoApp {
 
 	public static void main(String[] args) {
-		greetings();
+
+		if (args.length == 0) {
+			greetings();
+		} else if (args[0].equals("-l")) {
+			System.out.println("listing tasks");
+		} else if (args[0].equals("-a")) {
+			System.out.println("adding task");
+		} else if (args[0].equals("-r")) {
+			System.out.println("remove task");
+		} else if (args[0].equals("-c")) {
+			System.out.println("complete task");
+		} else System.out.println("Please try something else");
+
 	}
 
 	public static void greetings() {
